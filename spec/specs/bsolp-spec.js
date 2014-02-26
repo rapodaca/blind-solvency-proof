6fca25fc856641646518b0ad1aa4680eab389faa
Rich Apodaca <rapodaca@metamolecular.com>
2014-02-26 08:53:24 -0800

    Added specs for #extractPublicTree

--- spec/specs/bsolp-spec.js
+++ spec/specs/bsolp-spec.js
@@ -3,7 +3,7 @@ describe("bsolp", function() {
   beforeEach(function() {
     bsolp = require('bsolp');
   });
-  describe("#generate_private_tree", function() {
+  describe("#generatePrivateTree", function() {
     it("returns a tree with a root node given no accounts", function() {
       var accounts = [ ];
       var tree = bsolp.generatePrivateTree(accounts);
@@ -52,6 +52,63 @@ describe("bsolp", function() {
       var tree = bsolp.generatePrivateTree(accounts);
 
       expect(JSON.parse(tree.serialize())).toEqual([
+        { index : 0, data : { value : 1110, hash : 'Nsv9h1L8LS33eFOG+wPnQLBznnufOhVg6HjZ9gQuU+w=' } },
+        { index : 1, data : { value : 1010, hash : 'XzyPPOvHnJ37uFaEGaqMqwonpzBTPqg9EP1C8JvRPcI=' } },
+        { index : 2, data : { hash : 'satoshi', value : 100 } },
+        { index : 3, data : { hash : 'bernanke', value : 1000 } },
+        { index : 4, data : { hash : 'gmaxwell', value : 10 } }
+      ]);
+    });
+  });
+  describe("#extractPublicTree", function() {
+    it("returns public tree for one-account tree", function() {
+      var accounts = [
+        { user: 'satoshi', balance: 100 }
+      ];
+      var privateTree = bsolp.generatePrivateTree(accounts);
+      var publicTree = bsolp.extractPublicTree(privateTree, 'satoshi');
+
+      expect(JSON.parse(publicTree.serialize())).toEqual([
+        { index : 0, data : { hash : 'satoshi', value : 100 } }
+      ]);
+    });
+    it("returns public tree for two-account tree given root", function() {
+      var accounts = [
+        { user: 'satoshi', balance: 100 },
+        { user: 'bernanke', balance: 1000 }
+      ];
+      var privateTree = bsolp.generatePrivateTree(accounts);
+      var publicTree = bsolp.extractPublicTree(privateTree, 'MK5kTtAIJfzTBxnQd+iwMF+xinroegbHrBcVavPOP+c=');
+
+      expect(JSON.parse(publicTree.serialize())).toEqual([
+        { index : 0, data : { value : null, hash : 'MK5kTtAIJfzTBxnQd+iwMF+xinroegbHrBcVavPOP+c=' } }
+      ]);
+    });
+    it("returns nodes at and below height given 3 accounts and hash of internal vertex", function() {
+      var accounts = [
+        { user: 'satoshi', balance: 100 },
+        { user: 'bernanke', balance: 1000 },
+        { user: 'gmaxwell', balance: 10 }
+      ];
+      var privateTree = bsolp.generatePrivateTree(accounts);
+      var publicTree = bsolp.extractPublicTree(privateTree, 'satoshi');
+
+      expect(JSON.parse(publicTree.serialize())).toEqual([
+        { index : 0, data : { value : 1110, hash : 'Nsv9h1L8LS33eFOG+wPnQLBznnufOhVg6HjZ9gQuU+w=' } },
+        { index : 1, data : { value : 1010, hash : 'XzyPPOvHnJ37uFaEGaqMqwonpzBTPqg9EP1C8JvRPcI=' } },
+        { index : 2, data : { hash : 'satoshi', value : 100 } }
+      ]);
+    });
+    it("returns nodes at and below height given 3 accounts and hash of leaf", function() {
+      var accounts = [
+        { user: 'satoshi', balance: 100 },
+        { user: 'bernanke', balance: 1000 },
+        { user: 'gmaxwell', balance: 10 }
+      ];
+      var privateTree = bsolp.generatePrivateTree(accounts);
+      var publicTree = bsolp.extractPublicTree(privateTree, 'bernanke');
+
+      expect(JSON.parse(publicTree.serialize())).toEqual([
         { index : 0, data : { value : 1110, hash : 'Nsv9h1L8LS33eFOG+wPnQLBznnufOhVg6HjZ9gQuU+w=' } },
         { index : 1, data : { value : 1010, hash : 'XzyPPOvHnJ37uFaEGaqMqwonpzBTPqg9EP1C8JvRPcI=' } },
         { index : 2, data : { hash : 'satoshi', value : 100 } },
